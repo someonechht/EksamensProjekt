cad3ad439b21f6314e4332d16cc5c21d3a673a95
Jacob <Jacob@Rosborg.dk>
2018-03-06 08:30:24 +0100

    commit

--- serieltest.py
+++ serieltest.py
@@ -1,5 +1,16 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0')
-print(ser.name)
-ser.write(b'hello')
-ser.close()
+import threading
+
+
+def read_stream(ser):
+  while True:
+    if ser.inWaiting() > 0:
+      do_some_thing(ser.readline())
+
+def do_some_thing(line):
+  print(line)
+
+ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=0)
+
+thread = threading.Thread(target=read_stream, args=(serial_port,))
+thread.start()
