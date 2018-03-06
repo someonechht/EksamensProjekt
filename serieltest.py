a9e2d82be5fb0cf5c437693730ef5497e01463c7
Jacob <Jacob@Rosborg.dk>
2018-03-06 14:35:13 +0100

    commit

--- serieltest.py
+++ serieltest.py
@@ -5,16 +5,14 @@ import time
 
 def read_stream(ser):
   while ser.is_open:
-    if ser.in_waiting > 0:
-      do_some_thing(ser.readline())
+    do_some_thing(ser.readline())
 
 def do_some_thing(line):
   print(line.decode())
 
 ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=0)
 
-thread = threading.Thread(target=read_stream, args=(ser,))
-thread.daemon = True
+thread = threading.Thread(target=read_stream, args = (ser,), daemon = True)
 thread.start()
 
 while True:
