0ca9b512719f7846446b70f079e647f1181c144e
Jacob <Jacob@Rosborg.dk>
2018-03-06 13:05:35 +0100

    commit

--- cameraqr.py
+++ cameraqr.py
@@ -5,7 +5,6 @@ from PIL import Image
 import zbar
 import numpy
 
-running = True
 
 # Create a bytestream to store the image temporarily
 stream = BytesIO()
@@ -16,15 +15,14 @@ scanner = zbar.Scanner()
 with picamera.PiCamera() as camera:
 	# Scaledown the resolution by 100 or 10 on each axis
 	camera.resolution = (camera.resolution[0] // 6, camera.resolution[1] // 6)
-	# Turn it grayscale
-	camera.color_effects = (128,128) 
-	while running:
+	for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
 		print("Reading...")
 		# We want to reuse the byte stream
 		stream.seek(0)
 		camera.capture(stream, format='jpeg')
 		
 		pil = Image.open(stream)
+		pil = pil.convert('L')
 
 		results = scanner.scan(numpy.array(pil))
 
