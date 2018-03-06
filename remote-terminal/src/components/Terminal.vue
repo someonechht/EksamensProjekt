e84da63a77f6052edb87259fe78dec7b3b6cc3c3
Jacob <Jacob@Rosborg.dk>
2018-03-06 08:17:18 +0100

    commit

---
+++ remote-terminal/src/components/Terminal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="terminal">
+    <ul>
+      <div class="output">
+      <li v-for="(line, index) in lines" :key="index">{{ line }}</li>
+      </div>
+      <li>
+      <input class="input" v-model="input" type="text" @keyup.enter="exec()">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Terminal',
+  data: () => {
+    return {
+      lines: [],
+      input: ''
+    }
+  },
+  methods: {
+    exec () {
+      var args = this.input.split(' ')
+      if (args.lenght === 0) {
+        return
+      }
+
+      this.lines.push(this.input)
+
+      if (args[0] === 'clear') {
+        this.lines = []
+      }
+
+      this.input = ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.terminal
+  width: 30rem
+  margin: 0 auto
+  border: 1px solid black
+  text-align: left
+  ul
+    padding: .5em
+    list-style-type: none
+    margin: 0
+  .output
+    height: 15em
+    overflow: scroll
+.input
+  list-style-type: none
+  width: 100%
+  abox-sizing: border-box
+  border: none
+  outline: none
+  position: relative
+  bottom: 0
+</style>
